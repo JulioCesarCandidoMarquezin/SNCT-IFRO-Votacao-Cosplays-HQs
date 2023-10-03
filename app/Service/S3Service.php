08fa8a05af6d24ac6191e2c0d297fd2d938a7fac
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-10-02 20:03:46 -0400

    Adicionado método para pegar as urls das imagens, e agora são passadas url para o front

--- app/Service/S3Service.php
+++ app/Service/S3Service.php
@@ -2,7 +2,8 @@
 
 namespace App\Service;
 
-use App\DTO\S3\S3DTO;
+use App\DTO\S3\CosplayS3DTO;
+use App\DTO\S3\HqS3DTO;
 use Illuminate\Support\Facades\Storage;
 
 class S3Service
@@ -14,28 +15,51 @@ class S3Service
         $this->storage = Storage::disk('s3');
     }
 
-    public function getAll($prefix = null): array
+    public function getAll(string $prefix = null): array
     {
         return $prefix ? $this->storage->files($prefix) : $this->storage->allFiles();
     }
 
+    public function getAllUrlsFromPath(string $prefix = null): array
+    {
+        $files_path = $this->getAll($prefix);
+        foreach($files_path as $file_path) {
+            $files_url[] = $this->findOne($file_path);
+        }
+        return $files_url;
+    }
+
     public function findOne(string $path): string|null
     {
-        return $this->storage->get($path);
+        return $this->storage->url($path);
     }
 
-    public function store(S3DTO $dto): bool
+    public function store(CosplayS3DTO|HqS3DTO $dto): array
     {
-        return $this->storage->put($dto->path, $dto->file);
+        $store_path = $dto->path;
+        $paths = [$store_path];
+
+        foreach ($dto->images as $image) {
+            $fileName = $image->getClientOriginalName();
+            $path = $image->storeAs($dto->path, $fileName, 's3');            
+            $paths[] = $path;
+        }
+
+        return $paths;
     }
 
-    public function update(S3DTO $dto): bool
+    public function update(CosplayS3DTO|HqS3DTO $dto): bool
     {
-        return $this->storage->put($dto->path.$dto->name, $dto->file);
+        return $this->storage->put($dto->path, $dto->images);
     }
 
     public function delete(string $path): bool
     {
         return $this->storage->delete($path);
+    }
+
+    public function deleteDirectory(string $path): bool
+    {
+        return $this->storage->deleteDirectory($path);
     }
 }
